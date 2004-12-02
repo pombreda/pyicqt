4d34f499079fe98b4ae8c28cc659e094bba7517f
nobody <nobody@95155a4c-e240-0410-996a-fd041c3cf536>
2004-12-02 05:13:09 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_4'.

---
+++ tags/RELEASE_0_4/src/legacy/icqt.py
@@ -0,0 +1,409 @@
+# Copyright 2004 Daniel Henninger <jadestorm@nc.rr.com>
+# Licensed for distribution under the GPL version 2, check COPYING for details
+
+from twisted.web.microdom import Element
+from twisted.internet import protocol, reactor, defer
+#from twisted.protocols import oscar
+from tlib import oscar
+from twisted.python import log
+import config
+import utils
+import debug
+import sys, warnings, pprint
+
+
+
+#############################################################################
+# BOSConnection
+#############################################################################
+class B(oscar.BOSConnection):
+	def __init__(self,username,cookie,icqcon):
+		self.icqcon = icqcon
+		self.icqcon.bos = self
+		self.session = icqcon.session  # convenience
+		self.capabilities = [oscar.CAP_CHAT]
+		oscar.BOSConnection.__init__(self,username,cookie)
+
+	def initDone(self):
+		self.requestSelfInfo().addCallback(self.gotSelfInfo)
+		self.requestSSI().addCallback(self.gotBuddyList)
+		debug.log("B: initDone %s for %s" % (self.username,self.session.jabberID))
+
+	def updateBuddy(self, user):
+		from glue import icq2jid
+
+		debug.log("B: updateBuddy %s" % (user))
+		buddyjid = icq2jid(user.name)
+		ptype = None
+		show = None
+		status = None
+		if (user.flags.count("away")):
+			self.getAway(user.name).addCallback(self.sendAwayPresence, user)
+		else:
+			self.session.sendPresence(to=self.session.jabberID, fro=buddyjid, show=show, status=status, ptype=ptype)
+			self.icqcon.contacts.updateSSIContact(user.name, presence=ptype, show=show, status=status)
+
+	def offlineBuddy(self, user):
+		from glue import icq2jid
+
+		debug.log("B: offlineBuddy %s" % (user.name))
+		buddyjid = icq2jid(user.name)
+		show = None
+		status = None
+		ptype = "unavailable"
+		self.session.sendPresence(to=self.session.jabberID, fro=buddyjid, show=show, status=status, ptype=ptype)
+
+	def receiveMessage(self, user, multiparts, flags):
+		from glue import icq2jid
+
+		debug.log("B: receiveMessage %s %s %s %s %s" % (self.session.jabberID, self.name, user.name, multiparts, flags))
+		sourcejid = icq2jid(user.name)
+		text = multiparts[0][0]
+		encoding = "iso-8859-1"
+		debug.log("B: using encoding %s" % (encoding))
+		text = text.decode(encoding, "replace")
+		text = text.strip()
+		self.session.sendMessage(to=self.session.jabberID, fro=sourcejid, body=text, mtype="chat")
+
+	def receiveSendFileRequest(self, user, file, description, cookie):
+		debug.log("B: receiveSendFileRequest")
+
+
+	# Callbacks
+	def sendAwayPresence(self, msg, user):
+		from glue import icq2jid
+
+		buddyjid = icq2jid(user.name)
+		ptype = None
+		show = "away"
+		status = msg
+		self.session.sendPresence(to=self.session.jabberID, fro=buddyjid, show=show, status=status, ptype=ptype)
+		self.icqcon.contacts.updateSSIContact(user.name, presence=ptype, show=show, status=status)
+
+	def gotSelfInfo(self, user):
+		debug.log("B: gotSelfInfo: %s" % (user.__dict__))
+		self.name = user.name
+
+	def gotBuddyList(self, l):
+		debug.log("B: gotBuddyList: %s" % (str(l)))
+		self.ssigroups = list()
+		if (l is not None):
+			for g in l[0]:
+				debug.log("B: gotBuddyList found group %s" % (g.name))
+				self.ssigroups.append(g)
+				for u in g.users:
+					debug.log("B: got user %s from group %s" % (u.name, g.name))
+					self.icqcon.contacts.updateSSIContact(u.name, skipsave=True)
+		self.icqcon.contacts.saveXDBBuddies()
+		self.activateSSI()
+		self.setIdleTime(0)
+		self.clientReady()
+		self.session.ready = True
+		tmpjid = config.jid
+		if (self.session.registeredmunge):
+			tmpjid = tmpjid + "/registered"
+		self.session.sendPresence(to=self.session.jabberID, fro=tmpjid, show=self.icqcon.savedShow, status=self.icqcon.savedFriendly)
+		if (self.icqcon.savedShow in ["online", None]):
+			self.icqcon.setAway(None)
+		else:
+			self.icqcon.setAway(self.icqcon.savedFriendly)
+		self.requestOffline()
+
+
+#############################################################################
+# Oscar Authenticator
+#############################################################################
+class OA(oscar.OscarAuthenticator):
+	def __init__(self,username,password,icqcon,deferred=None,icq=1):
+		self.icqcon = icqcon
+		self.BOSClass = B
+		oscar.OscarAuthenticator.__init__(self,username,password,deferred,icq)
+
+	def connectToBOS(self, server, port):
+		c = protocol.ClientCreator(reactor, self.BOSClass, self.username, self.cookie, self.icqcon)
+		return c.connectTCP(server, int(port))
+
+
+
+#############################################################################
+# ICQConnection
+#############################################################################
+class ICQConnection:
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+		self.reactor = reactor
+		self.contacts = ICQContacts(self.session)
+		self.deferred = defer.Deferred()
+		self.deferred.addErrback(self.errorCallback)
+		#hostport = ("login.icq.com", 5238)
+		hostport = ("login.icq.com", 5190)
+		debug.log("ICQConnection: client creation for %s" % (self.session.jabberID))
+		self.oa = OA
+		self.creator = protocol.ClientCreator(self.reactor, self.oa, self.username, self.password, self, deferred=self.deferred, icq=1)
+		debug.log("ICQConnection: connect tcp")
+		self.creator.connectTCP(*hostport)
+
+		debug.log("ICQConnection: \"%s\" created" % (self.username))
+	
+	def setAway(self, awayMessage=None):
+		debug.log("ICQConnection: setAway %s" % (awayMessage))
+		if (not self.session.ready or not hasattr(self, "bos")):
+			return
+
+		self.bos.setAway(awayMessage)
+
+	def sendMessage(self, target, message):
+		from glue import jid2icq
+
+		scrnname = jid2icq(target)
+		debug.log("ICQConnection: sendMessage %s %s" % (scrnname, message))
+		encoded = message.encode("iso-8859-1", "replace")
+		if (self.session.ready and hasattr(self, "bos")):
+			self.bos.sendMessage(scrnname, encoded)
+		else:
+			debug.log("ICQConnection: not logged in yet")
+			if (hasattr(self.session, "jabberID")):
+				self.session.sendMessage(to=self.session.jabberID, fro=config.jid, body="You are not currently logged into this transport.  Please log in again.", mtype="chat")
+			return
+
+	def resendBuddies(self, resource):
+		from glue import icq2jid
+		debug.log("ICQConnection: resendBuddies %s" % (resource))
+		if (not hasattr(self, "contacts")):
+			return
+
+		for c in self.contacts.ssicontacts.keys( ):
+			debug.log("ICQConnection: resending buddy of %s" % (c))
+			jid = icq2jid(c)
+			show = self.contacts.ssicontacts[c]['show']
+			status = self.contacts.ssicontacts[c]['status']
+			ptype = self.contacts.ssicontacts[c]['presence']
+			self.session.sendPresence(to=self.session.jabberID, fro=jid, show=show, status=status, ptype=ptype)
+
+	def getvCard(self, vcard, user):
+		debug.log("ICQConnection: getvCard %s" % (user))
+		d = defer.Deferred()
+		self.bos.getMetaInfo(user).addCallback(self.gotvCard, user, vcard, d)
+		return d
+
+	def gotvCard(self, userinfo, uin, vcard, d):
+		debug.log("ICQConnection: gotvCard: %s" % (userinfo))
+
+		fn = vcard.addElement("FN")
+		fn.addContent(userinfo[1]+" "+userinfo[2])
+		n = vcard.addElement("N")
+		given = n.addElement("GIVEN")
+		given.addContent(userinfo[1])
+		family = n.addElement("FAMILY")
+		family.addContent(userinfo[2])
+		middle = n.addElement("MIDDLE")
+		nickname = vcard.addElement("NICKNAME")
+		nickname.addContent(userinfo[0])
+		email = vcard.addElement("EMAIL")
+		email.addElement("INTERNET")
+		email.addElement("PREF")
+		emailid = email.addElement("USERID")
+		emailid.addContent(userinfo[3])
+		jabberid = vcard.addElement("JABBERID")
+		jabberid.addContent(uin+"@"+config.jid)
+
+		d.callback(vcard)
+
+	def removeMe(self):
+		from glue import icq2jid
+		debug.log("ICQConnection: removeMe")
+		if (not hasattr(self, "bos")):
+			return
+
+		self.bos.stopKeepAlive()
+		self.bos.disconnect()
+
+		if (not hasattr(self, "contacts")):
+			return
+
+		for c in self.contacts.ssicontacts.keys( ):
+			if (self.contacts.ssicontacts[c]['presence'] == "unavailable"):
+				continue
+
+			debug.log("ICQConnection: sending offline for %s" % (c))
+			jid = icq2jid(c)
+			show = None
+			status = None
+			ptype = "unavailable"
+			self.session.sendPresence(to=self.session.jabberID, fro=jid, show=show, status=status, ptype=ptype)
+
+	def jabberSubscriptionReceived(self, to, subtype):
+		debug.log("ICQConnection: Session \"%s\" - jabberSubscriptionReceived(\"%s\", \"%s\")" % (self.session.jabberID, to, subtype))
+
+		def updatePresence(ptype): # Convenience
+			self.session.sendPresence(to=self.session.jabberID, fro=to, ptype=ptype)
+
+		if(to.find('@') > 0): # For contacts
+			from glue import jid2icq
+
+			userHandle = jid2icq(to)
+
+			if(subtype == "subscribe"):
+				# User wants to subscribe to contact's presence
+				debug.log("ICQConnection: Subscribe request received.")
+				def cb(arg=None):
+					updatePresence("subscribed")
+
+				if (not hasattr(self, "bos")):
+					debug.log("Not properly logged in yet")
+					if (hasattr(self.session, "jabberID")):
+						self.session.sendMessage(to=self.session.jabberID, fro=config.jid, body="You are not currently logged into this transport.  Please log in again.", mtype="chat")
+					return
+
+				savethisgroup = None
+				for g in self.bos.ssigroups:
+					if (g.name == "General"):
+						debug.log("Located group %s" % (g.name))
+						savethisgroup = g
+
+				if (savethisgroup is None):
+					debug.log("Need to add a new group")
+					return
+
+				newUser = oscar.SSIBuddy(userHandle)
+				newUserID = len(savethisgroup.users)+1
+				savethisgroup.addUser(newUserID, newUser)
+
+				debug.log("Adding item to SSI")
+				self.bos.startModifySSI()
+				self.bos.addItemSSI(newUser).addCallback(cb)
+				self.bos.modifyItemSSI(savethisgroup)
+				self.bos.endModifySSI()
+
+				self.contacts.updateSSIContact(userHandle)
+
+			elif(subtype == "subscribed"):
+				# The user has granted this contact subscription
+				debug.log("ICQConnection: Subscribed request received.")
+				pass
+
+			elif(subtype == "unsubscribe"):
+				# User wants to unsubscribe to this contact's presence. (User is removing the contact from their list)
+				debug.log("ICQConnection: Unsubscribe request received.")
+				def cb(arg=None):
+					updatePresence("unsubscribed")
+
+				savethisuser = None
+				for g in self.bos.ssigroups:
+					for u in g.users:
+						debug.log("Comparing %s and %s" % (u.name.lower(), userHandle))
+						if (u.name.lower() == userHandle):
+							debug.log("Located user %s" % (u.name))
+							savethisuser = u
+
+				if (savethisuser is None):
+					debug.log("Did not find user")
+					return
+
+				self.bos.startModifySSI()
+				self.bos.delItemSSI(savethisuser).addCallback(cb)
+				self.bos.endModifySSI()
+
+                        elif(subtype == "unsubscribed"):
+                                # The user wants to remove this contact's authorisation. Contact will no longer be able to see user
+				debug.log("ICQConnection: Unsubscribed request received.")
+				pass
+
+                else: # The user wants to change subscription to the transport
+                        if(subtype == "subscribe"):
+                                updatePresence("subscribed")
+
+                        elif(subtype == "subscribed"):
+                                return # Nothing to do
+
+                        elif(subtype == "unsubscribe" or subtype == "unsubscribed"):
+                                # They want to unregister. Ok, we can do that
+                                jid = self.session.jabberID
+				debug.log("Subscriptions: Session \"%s\" is about to be unregistered" % (jid))
+				self.session.pytrans.registermanager.removeRegInfo(jid)
+				debug.log("Subscriptions: Session \"%s\" has been unregistered" % (jid))
+
+
+	# Callbacks
+	def errorCallback(self, result):
+		debug.log("ICQConnection: errorCallback %s" % (result.getErrorMessage()))
+		errmsg = result.getErrorMessage()
+		errmsgs = errmsg.split("'")
+		message = "Authentication Error!"
+		if (errmsgs[1]):
+			message = message+"\n"+errmsgs[1]
+		if (errmsgs[3]):
+			message = message+"\n"+errmsgs[3]
+		tmpjid = config.jid
+		if (self.session.registeredmunge):
+			tmpjid = tmpjid + "/registered"
+		self.session.sendMessage(to=self.session.jabberID, fro=tmpjid, body=message, mtype="chat")
+
+		self.session.removeMe()
+
+
+
+#############################################################################
+# ICQContacts
+#############################################################################
+class ICQContacts:
+	def __init__(self, session):
+		self.session = session
+		self.ssicontacts = { }
+		self.xdbcontacts = self.getXDBBuddies()
+		self.xdbchanged = False
+
+	def updateSSIContact(self, contact, presence="unavailable", show=None, status=None, skipsave=False):
+		debug.log("ICQContacts: updating contact %s" % (contact.lower()))
+		self.ssicontacts[contact.lower()] = {
+			'presence': presence,
+			'show': show,
+			'status': status
+		}
+
+		if (not self.xdbcontacts.count(contact.lower())):
+			from glue import icq2jid
+			self.session.sendRosterImport(icq2jid(contact), "subscribe", "both", contact)
+			self.xdbcontacts.append(contact.lower())
+			self.xdbchanged = True
+			if (not skipsave):
+				self.saveXDBBuddies()
+
+	def getXDBBuddies(self):
+		debug.log("ICQContacts: getXDBBuddies %s %s" % (config.jid, self.session.jabberID))
+		bl = list()
+		result = self.session.pytrans.xdb.request(self.session.jabberID, "jabber:iq:roster")
+		if (result == None):
+			debug.log("ICQContacts: getXDBBuddies unable to get list, or empty")
+			return bl
+
+		for child in result.childNodes:
+			try:
+				if(child.tagName == "item"):
+					bl.append(child.attributes["jid"])
+			except AttributeError:
+				continue
+		return bl
+
+	def saveXDBBuddies(self):
+		debug.log("ICQContacts: setXDBBuddies %s %s" % (config.jid, self.session.jabberID))
+		if (not self.xdbchanged):
+			debug.log("ICQContacts: nothing has changed, no need to save")
+			return
+
+		newXDB = Element("query")
+		newXDB.namespace = "jabber:iq:roster"
+
+		for c in self.xdbcontacts:
+			try:
+				item = Element("item")
+				item.setAttribute("jid", c)
+				newXDB.appendChild(item)
+
+			except:
+				pass
+
+		self.session.pytrans.xdb.set(self.session.jabberID, "aimtrans:roster", newXDB)
+		self.xdbchanged = False
+		debug.log("ICQContacts: contacts saved")
---
+++ tags/RELEASE_0_4/src/session.py
@@ -0,0 +1,285 @@
+# Copyright 2004 James Bunton <james@delx.cjb.net>
+# Licensed for distribution under the GPL version 2, check COPYING for details
+
+import utils
+import legacy
+import jabw
+import debug
+import config
+import lang
+
+
+
+
+def makeSession(pytrans, jabberID, ulang, rosterID):
+	""" Tries to create a session object for the corresponding JabberID. Retrieves information
+	from XDB to create the session. If it fails, then the user is most likely not registered with
+	the transport """
+	debug.log("session: makeSession(\"%s\")" % (jabberID))
+	if(pytrans.sessions.has_key(jabberID)):
+		debug.log("session: makeSession() - removing existing session")
+		pytrans.sessions[jabberID].removeMe()
+	result = pytrans.registermanager.getRegInfo(jabberID)
+	if(result):
+		username, password, nickname = result
+		return Session(pytrans, jabberID, username, password, nickname, ulang, rosterID)
+	else:
+		return None
+
+
+
+class Session(jabw.JabberConnection):
+	""" A class to represent each registered user's session with the legacy network. Exists as long as there
+	is a Jabber resource for the user available """
+	
+	def __init__(self, pytrans, jabberID, username, password, nickname, ulang, rosterID):
+		""" Initialises the session object and connects to the legacy network """
+		jabw.JabberConnection.__init__(self, pytrans, jabberID)
+		debug.log("Session: Creating new session \"%s\"" % (jabberID))
+		
+		self.pytrans = pytrans
+		self.alive = True
+		self.ready = False # Only ready when we're logged into the legacy service
+		self.jabberID = jabberID # the JabberID of the Session's user
+		self.username = username # the legacy network ID of the Session's user
+		self.password = password
+		self.nickname = nickname
+		self.lang = ulang
+
+		if (rosterID.resource == "registered"):
+			self.registeredmunge = True
+		else:
+			self.registeredmunge = False
+		
+		self.show = None
+		self.status = None
+		
+		self.resourceList = []
+		self.groupchats = []
+		
+		self.legacycon = legacy.LegacyConnection(self.username, self.password, self)
+		self.pytrans.legacycon = self.legacycon
+		
+		if(config.sessionGreeting):
+			self.sendMessage(to=self.jabberID, fro=config.jid, body=lang.get(self.lang).sessionGreeting)
+		debug.log("Session: New session created \"%s\" \"%s\" \"%s\" \"%s\"" % (jabberID, username, password, nickname))
+	
+	
+	def removeMe(self):
+		""" Safely removes the session object, including sending <presence type="unavailable"/> messages for each legacy related item on the user's contact list """
+		# Send offline presence to Jabber ID
+		# Delete all objects cleanly
+		# Remove this Session object from the pytrans
+		
+		debug.log("Session: Removing \"%s\"" % (self.jabberID))
+		
+		# Mark as dead
+		self.alive = False
+		self.ready = False
+		
+		# Send offline presence to the user
+		if(self.pytrans):
+			tmpjid = config.jid
+			if (self.registeredmunge):
+				tmpjid = tmpjid + "/registered"
+			self.sendPresence(to=self.jabberID, fro=tmpjid, ptype="unavailable")
+		
+		# Clean up stuff on the legacy service end (including sending offline presences for all contacts)
+		if(self.legacycon):
+			self.legacycon.removeMe()
+			self.legacycon = None
+		
+		# Remove any groupchats we may be in
+		for groupchat in utils.copyList(self.groupchats):
+			groupchat.removeMe()
+		
+		if(self.pytrans):
+			# Remove us from the session list
+			del self.pytrans.sessions[self.jabberID]
+			# Clean up the no longer needed reference
+			self.pytrans = None
+		
+		debug.log("Session: Completed removal \"%s\"" % (self.jabberID))
+	
+	def updateNickname(self, nickname):
+		self.nickname = nickname
+		self.setStatus(self.show, self.status)
+	
+	def setStatus(self, show, status):
+		self.show = show
+		self.status = status
+		self.legacycon.setStatus(show, status)
+	
+	def sendNotReadyError(self, source, resource, dest, body):
+		self.sendErrorMessage(source + '/' + resource, dest, "wait", "not-allowed", lang.get(self.lang).waitForLogin, body)
+	
+	def findGroupchat(self, to):
+		pos = to.find('@')
+		if(pos > 0):
+			roomID = to[:pos]
+		else:
+			roomID = to
+		
+		for groupchat in self.groupchats:
+			if(groupchat.ID == roomID):
+				return groupchat
+		
+		return None
+		
+	def messageReceived(self, source, resource, dest, destr, mtype, body):
+		if(not self.ready):
+			self.sendNotReadyError(source, resource, dest, body)
+			return
+		
+		# Sends the message to the legacy translator
+		groupchat = self.findGroupchat(dest)
+		if(groupchat):
+			# It's for a groupchat
+			if(destr and len(destr) > 0):
+				self.sendMessage(to=(source + "/" + resource), fro=dest, body=lang.get(self.lang).groupchatPrivateError)
+			else:
+				debug.log("Session: Message received for groupchat \"%s\" \"%s\"" % (self.jabberID, groupchat.ID))
+				groupchat.sendMessage(body)
+		else:
+			debug.log("Session: messageReceived(), passing onto legacycon.sendMessage()")
+			self.legacycon.sendMessage(dest, body)
+	
+	def inviteReceived(self, source, resource, dest, destr, roomjid):
+		if(not self.ready):
+			self.sendNotReadyError(source, resource, dest, roomjid)
+			return
+		
+		groupchat = self.findGroupchat(roomjid)
+		if(groupchat):
+			debug.log("Session: inviteReceived(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\")" % (source, resource, dest, destr, roomjid))
+			groupchat.sendContactInvite(dest)
+	
+	def presenceReceived(self, source, resource, to, tor, priority, ptype, show, status):
+		# Checks resources and priorities so that the highest priority resource always appears as the
+		# legacy services status. If there are no more resources then the session is deleted
+		# Additionally checks if the presence is to a groupchat room
+		groupchat = self.findGroupchat(to)
+		if(groupchat):
+			# It's for a groupchat
+			if(ptype == "unavailable"):
+				# Kill the groupchat
+				debug.log("Session: Presence received to kill groupchat \"%s\" \"%s\"" % (self.jabberID, groupchat.ID))
+				groupchat.removeMe()
+			else:
+				if(source == self.jabberID):
+					debug.log("Session: Presence for groupchat \"%s\" \"%s\"" % (self.jabberID, groupchat.ID))
+					if(ptype == "error"):
+						groupchat.removeMe()
+					else:
+						groupchat.userJoined(tor)
+				else:
+					debug.log("Session: Sending error presence for groupchat (user not allowed) \"%s\" \"%s\"" % (self.jabberID, groupchat.ID))
+					self.sendPresence(to=(source + "/" + resource), fro=to, ptype="error")
+		
+		elif(legacy.isGroupJID(to) and to != config.jid and ptype != "error"):
+			# It's a new groupchat
+			gcID = to[:to.find('@')] # Grab the room name
+			debug.log("Session: Creating a new groupchat \"%s\" \"%s\"" % (self.jabberID, gcID))
+			groupchat = legacy.LegacyGroupchat(self, resource, gcID) # Creates an empty groupchat
+			groupchat.userJoined(tor)
+		
+		else:
+			# Not for groupchat
+			self.handleResourcePresence(source, resource, to, tor, priority, ptype, show, status)
+
+		
+	def handleResourcePresence(self, source, resource, to, tor, priority, ptype, show, status):
+		if(to.find('@') > 0): return # Ignore presence packets sent to users
+		r = SessionResource(resource, show, status, priority)
+		if(ptype == None):
+			# Check to see if this is a new resource, if so, send out the contact list
+			debug.log("Session: Presence change received \"%s\"" % (self.jabberID))
+			if(self.resourceList.count(r) == 0):
+				debug.log("Session: New resource online \"%s\" \"%s\" \"%s\"" % (self.jabberID, resource, priority))
+				self.legacycon.newResourceOnline(resource)
+			
+			# We must check the resource and priority, only interested in the highest priority
+			higher = False
+			oldList = utils.copyList(self.resourceList)
+			self.resourceList = []
+			for checkR in oldList:
+				if(checkR > r):
+					higher = True
+				if(checkR == r):
+					self.resourceList.append(r)
+				else:
+					self.resourceList.append(checkR)
+			if(self.resourceList.count(r) == 0):
+				self.resourceList.append(r) # Make sure it's in there
+			
+			if(not higher):
+				# No higher resource was found. So we must be the highest!
+				# This means we must update the presence on the legacy network
+				debug.log("Session: \"%s\" Updating status on legacy network \"%s\" \"%s\"" % (self.jabberID, show, status))
+				self.setStatus(show, status)
+		
+		elif(ptype == "unavailable"):
+			# This resource has gone offline, so we need to remove it from the list, and find the next highest
+			# resource to use to update our status on the legacy network
+			debug.log("Session: Unavailable presence received from \"%s\". Looking for other resources" % (self.jabberID))
+			lst = utils.copyList(self.resourceList)
+			self.resourceList = []
+			for i in lst:
+				if(r != i):
+					self.resourceList.append(i)
+			
+			highest = None
+			for checkR in self.resourceList:
+				if(checkR > highest): # comparisons against None are always True
+					highest = checkR
+			if(highest == None):
+				# There are no resources left, so offline this session
+				debug.log("Session: No resources left \"%s\"" % (source))
+				self.removeMe()
+			else:
+				# Update the legacy network's status with the highest leftover resource
+				debug.log("Session: New resource \"%s\" found for session \"%s\"" % (highest.resource, self.jabberID))
+				self.setStatus(highest.show, highest.status)
+	
+	
+	def subscriptionReceived(self, to, subtype):
+		""" Sends the subscription request to the legacy services handler """
+		debug.log("Session: \"%s\" subscriptionReceived(), passing onto legacycon.jabberSubscriptionReceived()" % (self.jabberID))
+		self.legacycon.jabberSubscriptionReceived(to, subtype)
+	
+
+
+
+
+
+
+
+
+class SessionResource:
+	""" A convienence class to allow comparisons of Jabber resources """
+	def __init__(self, resource, show=None, status=None, priority=None):
+		self.resource = resource
+		self.show = show
+		self.status = status
+		self.priority = 0
+		try:
+			self.priority = int(priority)
+		except: pass
+	
+	def __eq__(self, other):
+		if(other):
+			return (self.resource == other.resource)
+		else:
+			return False
+	
+	def __ne__(self, other):
+		if(other):
+			return (self.resource != other.resource)
+		else:
+			return False
+	
+	def __cmp__(self, other):
+		if(other):
+			return self.priority.__cmp__(other.priority)
+		else:
+			return 1
