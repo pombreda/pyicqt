eb2169740a8bac880356d57ebf59e038125a74ad
jadestorm <jadestorm@95155a4c-e240-0410-996a-fd041c3cf536>
2006-05-31 18:32:43 +0000

    Locking in 0.7b release.

---
+++ tags/RELEASE_0_7b/src/main.py
@@ -0,0 +1,465 @@
+# Copyrigh 2004-2006 Daniel Henninger <jadestorm@nc.rr.com>
+# Licensed for distribution under the GPL version 2, check COPYING for details
+
+import utils
+import getopt
+import sys
+import os
+import shutil
+import time
+import codecs
+reload(sys)
+sys.setdefaultencoding('utf-8')
+del sys.setdefaultencoding
+sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
+
+if __name__ == "__main__":
+	print "The transport can no longer be started from main.py.  Please use"
+	print "PyICQt.py from the root of the distribution instead."
+	sys.exit(0)
+
+from tlib.twistwrap import VersionNumber
+if VersionNumber(sys.version[:3]) < VersionNumber("2.2"):
+	print("You are using version %s of Python, at least 2.2 is required." % (sys.version[:3]))
+	sys.exit(0)
+
+import config
+import xmlconfig
+conffile = "config.xml"
+profilelog = None
+options = {}
+daemonizeme = False
+opts, args = getopt.getopt(sys.argv[1:], "bc:o:dDgtl:p:h", ["background", "config=", "option=", "debug", "Debug", "garbage", "traceback", "log=", "profile=", "help"])
+for o, v in opts:
+	if o in ("-c", "--config"):
+		conffile = v
+	elif o in ("-p", "--profile"):
+		profilelog = v
+	elif o in ("-b", "--background"):
+                daemonizeme = True
+	elif o in ("-d", "--debug"):
+		config.debugLevel = "2"
+	elif o in ("-D", "--Debug"):
+		config.debugLevel = "3"
+	elif o in ("-g", "--garbage"):
+		import gc
+		gc.set_debug(gc.DEBUG_LEAK|gc.DEBUG_STATS)
+	elif o in ("-t", "--traceback"):
+		config.debugLevel = "1"
+	elif o in ("-l", "--log"):
+		config.debugFile = v
+	elif o in ("-o", "--option"):
+		var, setting = v.split("=", 2)
+		options[var] = setting
+	elif o in ("-h", "--help"):
+		print "./PyICQt [options]"
+		print "   -h                  print this help"
+		print "   -b                  daemonize/background transport"
+		print "   -c <file>           read configuration from this file"
+		print "   -d                  print debugging output"
+		print "   -D                  print extended debugging output"
+		print "   -g                  print garbage collection output"
+		print "   -t                  print debugging only on traceback"
+		print "   -l <file>           write debugging output to file"
+		print "   -o <var>=<setting>  set config var to setting"
+		sys.exit(0)
+#reload(debug)
+
+#if config.extendedDebugOn:
+#	from twisted.python import log
+#	if debug.debugFile:
+#		log.startLogging(debug.debugFile, 0)
+#	else:
+#		log.startLogging(sys.stdout, 0)
+xmlconfig.Import(conffile, options)
+
+def reloadConfig(a, b):
+	# Reload default config and then process conf file
+	reload(config)
+	xmlconfig.Import(conffile, None)
+	debug.reloadConfig()
+
+# Set SIGHUP to reload the config file
+if os.name == "posix":
+	import signal
+	signal.signal(signal.SIGHUP, reloadConfig)
+
+if config.reactor:
+	# They picked their own reactor. Lets install it.
+	del sys.modules["twisted.internet.reactor"]
+	if config.reactor == "epoll":
+		from twisted.internet import epollreactor
+		epollreactor.install()
+	elif config.reactor == "poll":
+		from twisted.internet import pollreactor
+		pollreactor.install()
+	elif config.reactor == "kqueue":
+		from twisted.internet import kqreactor
+		kqreactor.install()
+	elif len(config.reactor) > 0:
+		print "Unknown reactor: ", config.reactor, ". Using default, select(), reactor."
+else:
+	# Find the best reactor
+	del sys.modules["twisted.internet.reactor"]
+	try:
+		from twisted.internet import epollreactor as bestreactor
+		#LogEvent(INFO, "", "Found and using epollreactor")
+	except ImportError:
+		try:
+			from twisted.internet import kqreactor as bestreactor
+			#LogEvent(INFO, "", "Found and using kqreactor")
+		except ImportError:
+			try:
+				from twisted.internet import pollreactor as bestreactor
+				#LogEvent(INFO, "", "Found and using pollreactor")
+			except ImportError:
+				try:
+					from twisted.internet import default as bestreactor
+					print "Unable to install any good reactors (kqueue, epoll, poll)."
+					print "We fell back to using select. You may have scalability problems."
+					print "This reactor will not support more than 1024 connections at a time."
+				except ImportError:
+					print "Unable to find a reactor.\nExiting..."
+					sys.exit(1)
+	bestreactor.install()
+
+
+from twisted.internet import reactor, task
+from twisted.internet.defer import Deferred
+import twisted.python.log
+from tlib.twistwrap import component, jid, Element
+
+from debug import LogEvent, INFO, WARN, ERROR
+import debug
+import xdb
+import avatar
+import session
+import jabw
+import disco
+import register
+import misciq
+import legacy
+import lang
+import globals
+
+
+
+class PyTransport(component.Service):
+	j2bound = 0
+	def __init__(self):
+		LogEvent(INFO)
+		# Discovery, as well as some builtin features
+		self.discovery = disco.ServerDiscovery(self)
+		self.discovery.addIdentity("gateway", legacy.id, legacy.name, config.jid)
+		self.discovery.addFeature(globals.XHTML, None, "USER")
+
+		self.xdb = xdb.XDB(config.jid)
+		if not config.disableAvatars: self.avatarCache = avatar.AvatarCache()
+		self.registermanager = register.RegisterManager(self)
+		self.adHocCommands = misciq.AdHocCommands(self)
+		self.gatewayTranslator = misciq.GatewayTranslator(self)
+		self.versionTeller = misciq.VersionTeller(self)
+		self.pingService = misciq.PingService(self)
+		self.vCardFactory = misciq.VCardFactory(self)
+		#self.searchFactory = misciq.SearchFactory(self)
+		if not config.disableAvatars: self.IqAvatarFactory = misciq.IqAvatarFactory(self)
+		self.statistics = misciq.Statistics(self)
+		self.connectUsers = misciq.ConnectUsers(self)
+		legacy.addCommands(self)
+		self.startTime = int(time.time())
+
+		self.xmlstream = None
+		self.sessions = {}
+		
+		# Message IDs
+		self.messageID = 0
+		
+		self.loopCall = task.LoopingCall(self.loopCall)
+		self.loopCall.start(60.0)
+		
+		# Display active sessions if debug mode is on
+		#if config.debugOn:
+		#	twisted.python.log.addObserver(self.exceptionLogger)
+		
+	
+	def removeMe(self):
+		LogEvent(INFO)
+		for session in self.sessions.copy():
+			self.sessions[session].removeMe()
+
+	#def exceptionLogger(self, *kwargs):
+	#	if len(config.debugLog) > 0:
+	#		kwargs = kwargs[0]
+	#		if kwargs.has_key("failure"):
+	#			failure = kwargs["failure"]
+	#			failure.printTraceback(debug) # Pass debug as a pretend file object because it implements the write method
+	#			if config.debugLog:
+	#				debug.flushDebugSmart()
+	#				print "Exception occured! Check the log!"
+
+	def makeMessageID(self):
+		self.messageID += 1
+		return str(self.messageID)
+	
+	def loopCall(self):
+		numsessions = len(self.sessions)
+
+		#if config.debugOn and numsessions > 0:
+		#	debug.log("Sessions:")
+		#	for key in self.sessions:
+		#		debug.log("\t" + self.sessions[key].jabberID)
+		#		for res in self.sessions[key].resourceList:
+		#			debug.log("\t\t" + res)
+        
+		self.statistics.stats["Uptime"] = int(time.time()) - self.startTime
+		legacy.updateStats(self.statistics)
+		if numsessions > 0:
+			oldDict = self.sessions.copy()
+			self.sessions = {}
+			for key in oldDict:
+				session = oldDict[key]
+				if not session.alive:
+					LogEvent(WARN, "", "Ghost session found")
+					# Don't add it to the new dictionary. Effectively removing it
+				else:
+					self.sessions[key] = session
+	
+	def componentConnected(self, xmlstream):
+		LogEvent(INFO)
+		self.xmlstream = xmlstream
+		self.xmlstream.addObserver("/iq", self.discovery.onIq)
+		self.xmlstream.addObserver("/presence", self.onPresence)
+		self.xmlstream.addObserver("/message", self.onMessage)
+		self.xmlstream.addObserver("/bind", self.onBind)
+		self.xmlstream.addObserver("/route", self.onRouteMessage)
+		self.xmlstream.addObserver("/error[@xmlns='http://etherx.jabber.org/streams']", self.streamError)
+		if config.useXCP and config.compjid:
+			pres = Element((None, "presence"))
+			pres.attributes["to"] = "presence@-internal"
+			pres.attributes["from"] = config.compjid
+			x = pres.addElement("x")
+			x.attributes["xmlns"] = "http://www.jabber.com/schemas/component-presence.xsd"
+			x.attributes["xmlns:config"] = "http://www.jabber.com/config"
+			x.attributes["config:version"] = "1"
+			x.attributes["protocol-version"] = "1.0"
+			x.attributes["config-ns"] = legacy.url + "/component"
+			self.send(pres)
+		if config.saslUsername and config.useJ2Component:
+			LogEvent(INFO, "", "J2C binding to %r" % config.jid)
+			bind = Element((None,"bind"))
+			#bind.attributes["xmlns"] = "http://jabberd.jabberstudio.org/ns/component/1.0"
+			bind.attributes["name"] = config.jid
+			self.send(bind)
+		if config.saslUsername and config.useJ2Component:
+			self.j2bound = 1
+
+		self.sendInvitations()
+
+	def send(self, obj):
+		if self.j2bound == 1 and type(obj) == Element:
+			to = obj.getAttribute("to")
+			route = Element((None,"route"))
+			#route.attributes["xmlns"] = "http://jabberd.jabberstudio.org/ns/component/1.0"
+			route.attributes["from"] = config.jid
+			route.attributes["to"] = jid.JID(to).host
+			route.addChild(obj)
+			obj.attributes["xmlns"] = "jabber:client"
+			component.Service.send(self,route.toXml())
+		else:
+			if type(obj) == Element:
+				obj = obj.toXml()
+			component.Service.send(self,obj)
+	
+	def componentDisconnected(self):
+		LogEvent(INFO)
+		self.xmlstream = None
+		self.j2bound = 0
+
+	def onRouteMessage(self, el):
+		LogEvent(INFO)
+		for child in el.elements():
+			if child.name == "message": 
+				self.onMessage(child)
+			elif child.name == "presence":
+				# Ignore any presence broadcasts about other XCP components
+				if child.getAttribute("to") and child.getAttribute("to").find("@-internal") > 0: return
+				self.onPresence(child)
+			elif child.name == "iq":
+				self.discovery.onIq(child)
+			elif child.name == "bind": 
+				self.onBind(child)
+
+	def onBind(self, el):
+		LogEvent(INFO)
+
+	def streamError(self, errelem):
+		LogEvent(INFO)
+		self.xmlstream.streamError(errelem)
+
+	def streamEnd(self, errelem):
+		LogEvent(INFO)
+	
+	def onMessage(self, el):
+		fro = el.getAttribute("from")
+		to = el.getAttribute("to")
+		mtype = el.getAttribute("type")
+		try:
+			froj = jid.JID(fro)
+		except Exception, e:
+			LogEvent(WARN, "", "Failed stringprep")
+			return
+		if self.sessions.has_key(froj.userhost()):
+			self.sessions[froj.userhost()].onMessage(el)
+		elif mtype != "error":
+			ulang = utils.getLang(el)
+			body = None
+			for child in el.elements():
+				if child.name == "body":
+					body = child.__str__()
+			LogEvent(INFO, "", "Sending error response to a message outside of seession")
+			jabw.sendErrorMessage(self, fro, to, "auth", "not-authorized", lang.get("notloggedin", ulang), body)
+	
+	def onPresence(self, el):
+		fro = el.getAttribute("from")
+		to = el.getAttribute("to")
+		# Ignore any presence broadcasts about other JD2 components
+		if to == None: return
+		try:
+			froj = jid.JID(fro)
+			toj = jid.JID(to)
+		except Exception, e:
+			LogEvent(WARN, "", "Failed stringprep")
+			return
+
+		if self.sessions.has_key(froj.userhost()):
+			self.sessions[froj.userhost()].onPresence(el)
+		else:
+			ulang = utils.getLang(el)
+			ptype = el.getAttribute("type")
+			if to.find('@') < 0:
+				# If the presence packet is to the transport (not a user) and there isn't already a session
+				if not ptype: # Don't create a session unless they're sending available presence
+					LogEvent(INFO, "", "Attempting to create a new session")
+					s = session.makeSession(self, froj.userhost(), ulang, toj)
+					if s:
+						self.sessions[froj.userhost()] = s
+						LogEvent(INFO, "", "New session created")
+						# Send the first presence
+						s.onPresence(el)
+						# Get the capabilities
+						s.getCapabilities(el)
+					else:
+						LogEvent(INFO, "", "Failed to create session")
+						jabw.sendMessage(self, to=froj.userhost(), fro=config.jid, body=lang.get("notregistered", ulang))
+				
+				elif ptype != "error":
+					LogEvent(INFO, "", "Sending unavailable presence to non-logged in user")
+					pres = Element((None, "presence"))
+					pres.attributes["from"] = to
+					pres.attributes["to"] = fro
+					pres.attributes["type"] = "unavailable"
+					self.send(pres)
+					return
+			
+			elif ptype and (ptype.startswith("subscribe") or ptype.startswith("unsubscribe")):
+				# They haven't logged in, and are trying to change subscription to a user
+				# Lets log them in and then do it
+				LogEvent(INFO, "", "New session created")
+				s = session.makeSession(self, froj.userhost(), ulang, toj)
+				if s:
+					self.sessions[froj.userhost()] = s
+					LogEvent(INFO, "", "New session created")
+					# Tell the session there's a new resource
+					s.handleResourcePresence(froj.userhost(), froj.resource, toj.userhost(), toj.resource, 0, None, None, None)
+					# Send this subscription
+					s.onPresence(el)
+
+	def sendInvitations(self):              
+		if config.enableAutoInvite:
+			for jid in self.xdb.getRegistrationList():
+				LogEvent(INFO, "", "Inviting %r" % jid)
+				jabw.sendPresence(self, jid, config.jid, ptype="probe")
+				jabw.sendPresence(self, jid, "%s/registered" % (config.jid), ptype="probe")
+
+
+
+class App:
+	def __init__(self):
+		# Check that there isn't already a PID file
+		if config.pid:
+			if os.path.isfile(config.pid):
+				pf = open(config.pid)
+				pid = int(str(pf.readline().strip()))
+				pf.close()
+				if os.name == "posix":
+					try:
+						os.kill(pid, signal.SIGHUP)
+						self.alreadyRunning()
+					except OSError:
+						# The process is still up
+						pass
+				else:
+					self.alreadyRunning()
+
+			# Create a PID file
+			pid = str(os.getpid())
+			pf = file(config.pid,'w')
+			pf.write("%s\n" % pid);
+			pf.close()
+
+		# Initialize debugging
+		debug.reloadConfig()
+
+		jid = config.jid
+		if config.useXCP and config.compjid: jid = config.compjid
+
+		if config.saslUsername:
+			import sasl
+			self.c = sasl.buildServiceManager(jid, config.saslUsername, config.secret, "tcp:%s:%s" % (config.mainServer, config.port))
+		else:
+			self.c = component.buildServiceManager(jid, config.secret, "tcp:%s:%s" % (config.mainServer, config.port))
+		self.transportSvc = PyTransport()
+		self.transportSvc.setServiceParent(self.c)
+		self.c.startService()
+
+		reactor.addSystemEventTrigger('before', 'shutdown', self.shuttingDown)
+
+	def alreadyRunning(self):
+		print "There is already a transport instance running with this configuration."
+		print "Exiting..."
+		sys.exit(1)
+
+	def shuttingDown(self):
+		self.transportSvc.removeMe()
+		if config.pid:
+			def cb(ignored=None):
+				os.remove(config.pid)
+			d = Deferred()
+			d.addCallback(cb)
+			reactor.callLater(3.0, d.callback, None)
+			return d
+
+
+
+def main():
+	if daemonizeme:
+		import daemonize
+		if len(config.debugFile) > 0:
+			daemonize.daemonize(stdout=config.debugFile,stderr=config.debugFile)
+		else:
+			daemonize.daemonize()
+
+	# Do any auto-update stuff
+	xdb.housekeep()
+
+	app = App()
+	if hasattr(config, "webport") and config.webport:
+		try:
+			from nevow import appserver
+			import web
+			site = appserver.NevowSite(web.WebInterface(pytrans=app.transportSvc))
+			reactor.listenTCP(int(config.webport), site)
+			LogEvent(INFO, "", "Web interface activated")
+		except:
+			LogEvent(WARN, "", "Unable to start web interface.  Either Nevow is not installed or you need a more recent version of Twisted.  (>= 2.0.0.0)")
+	reactor.run()
---
+++ tags/RELEASE_0_7b/tools/managessi.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+import sys
+sys.path.append("../src/tlib")
+import oscar
+from twisted.internet import protocol, reactor
+import getpass
+import binascii
+import cmd
+import os
+import getopt
+
+SN = None
+PASS = None
+opts, args = getopt.getopt(sys.argv[1:], "ds:p:h", ["debug", "screenname", "password", "help"])
+for o, v in opts:
+	if o in ("-d", "--debug"):
+		from twisted.python import log
+		log.startLogging(sys.stdout, 0)
+	elif o in ("-s", "--screenname"):
+		SN = v
+	elif o in ("-p", "--password"):
+		PASS = v
+	elif o in ("-h", "--help"):
+		print "Available command line args are:"
+		print "   -d/--debug           enable debugging output"
+		print "   -s/--screenname      screenname/uin to log in as"
+		print "   -p/--password        password to authenticate with"
+		print "   -h/--help            display this help output"
+		os._exit(0)
+
+
+if not SN:
+	SN = raw_input('Username: ')
+if not PASS:
+	PASS = getpass.getpass('Password: ')
+if SN[0].isdigit():
+	icqMode = 1
+	hostport = ('login.icq.com', 5238)
+else:
+	hostport = ('login.oscar.aol.com', 5190)
+	icqMode = 0
+
+class SSICmd(cmd.Cmd):
+	def __init__(self, bos):
+		self.bos = bos
+		cmd.Cmd.__init__(self)
+	def do_delete(self, rest):
+		pcs = rest.split()
+		if len(pcs) < 1:
+			self.help_delete()
+		elif pcs[0] == 'buddy':
+			if len(pcs) != 3:
+				print "You need to specify both a group and buddy id."
+				self.help_delete()
+			else:
+				self.bos.removeBuddy(int(pcs[1]), int(pcs[2]))
+		elif pcs[0] == 'group':
+			if len(pcs) != 2:
+				print "You need to specify a group id."
+				self.help_delete()
+			else:
+				self.bos.removeGroup(int(pcs[1]))
+		elif pcs[0] == 'icon':
+			if len(pcs) != 2:
+				print "You need to specify a icon id."
+				self.help_delete()
+			else:
+				self.bos.removeIcon(int(pcs[1]))
+		else:
+			self.help_delete()
+	def help_delete(self):
+		print "delete buddy (group id) (buddy id): removes the specified buddy from the specified group"
+		print "delete group (group id): removes the specified group"
+		print "delete icon (icon id): removes the specified icon"
+	def do_show(self, rest):
+		pcs = rest.split()
+		if len(pcs) != 1:
+			self.help_show()
+			return
+		if pcs[0] == "buddies":
+			self.bos.showBuddyList()
+		elif pcs[0] == "icons":
+			self.bos.showIconList()
+		else:
+			self.help_show()
+	def help_show(self):
+		print "show (type): displays information for one of the following types"
+		print "   buddies       list of groups and buddies"
+		print "   icons         list of buddy icons"
+	def do_quit(self, rest):
+		self.bos.stopKeepAlive()
+		self.bos.disconnect()
+		print "Done."
+		os._exit(0)
+	def help_quit(self): print "quit: exits the program"
+	def help_help(self): print "help (command): explains the specified command"
+
+class B(oscar.BOSConnection):
+	capabilities = []
+	ssi = []
+	def initDone(self):
+		self.requestSSI().addCallback(self.gotBuddyList)
+	def gotBuddyList(self, l):
+		self.ssi = l
+		self.clientReady()
+		SSICmd(self).cmdloop()
+	def showBuddyList(self):
+		if self.ssi is not None and self.ssi[0] is not None:
+			for g in self.ssi[0]:
+				print "Group[%0.5d]: %s" % (g.groupID,g.name)
+				for u in g.users:
+					print "\tMember[%0.5d]: %s" % (u.buddyID,u.name)
+	def showIconList(self):
+		if self.ssi is not None and self.ssi[5] is not None:
+			for i in self.ssi[5]:
+				print "Icon[%d,%d,%s]: %s" % (i.groupID,i.buddyID,i.name,binascii.hexlify(i.iconSum))
+	def removeBuddy(self, groupid, buddyid):
+		savethisuser = None
+		savethisgroup = None
+		for g in self.ssi[0]:
+			if g.groupID == groupid:
+				for u in g.users:
+					if u.buddyID == buddyid:
+						savethisuser = u
+						savethisgroup = g
+						break
+				break
+		if savethisuser is None:
+			print "Unable to locate buddy id %d in group id %d." % (buddyid, groupid)
+			return
+		self.startModifySSI()
+		de = self.delItemSSI(savethisuser)
+		self.endModifySSI()
+		savethisgroup.users.remove(savethisuser)
+		del savethisgroup.usersToID[savethisuser]
+		print "Removed buddy with id %d from group with id %d" % (buddyid, groupid)
+	def removeGroup(self, groupid):
+		savethisgroup = None
+		for g in self.ssi[0]:
+			if g.groupID == groupid:
+				savethisgroup = g
+				break
+		if savethisgroup is None:
+			print "Unable to locate group id %d." % (groupid)
+			return
+		self.startModifySSI()
+		de = self.delItemSSI(savethisgroup)
+		self.endModifySSI()
+		self.ssi[0].remove(savethisgroup)
+		print "Removed group with id %d" % groupid
+	def removeIcon(self, iconid):
+		savethisicon = None
+		for i in self.ssi[5]:
+			if i.buddyID == iconid:
+				savethisicon = i
+				break
+		if savethisicon is None:
+			print "Unable to locate icon id %d." % (iconid)
+			return
+		self.startModifySSI()
+		de = self.delItemSSI(savethisicon)
+		self.endModifySSI()
+		self.ssi[5].remove(savethisicon)
+		print "Removed icon with id %d" % iconid
+
+class OA(oscar.OscarAuthenticator):
+	BOSClass = B
+
+protocol.ClientCreator(reactor, OA, SN, PASS, icq=icqMode).connectTCP(*hostport)
+reactor.run()
+print "Done."
